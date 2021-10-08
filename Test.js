0c76889bcba16804b9968528684b075c3760b23f
rungtom003 <rungchai990@gmail.com>
2021-10-08 14:19:39 +0700

    d

--- Test.js
+++ Test.js
@@ -1,16 +1,14 @@
 const keygen = require("keygenerator");
 const axios = require('axios');
-let message = "เเจ้งซ่อม:ทดสอบการเเจ้งซ่อม"
-let subMessage = message.slice(0,10);
+let message = "แจ้งซ่อม:ทดสอบการเเจ้งซ่อม"
+let subMessage = message.slice(0,9);
 let payload = message.slice(10);
-const URL = `https://script.google.com/macros/s/AKfycbyE8s1aX8unPX4osRMmMgbnmFVMLtgu2hYdNChBEPM1egjz8TwDFqmOvMfNpQsws8TEGg/exec?action=addMessage`
-axios.post(URL, {
-    id: `${keygen._()}`,
-    message: `${payload}`
-  })
-  .then(function (response) {
-    console.log(response.data);
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
+console.log(subMessage)
+if(subMessage === "แจ้งซ่อม:")
+{
+    console.log("ผ่าน")
+}
+else
+{
+    console.log("ไม่ผ่าน")
+}
