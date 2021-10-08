2bbc44c6bbdee25efbe2741683e41bf86a22f4d7
rungtom003 <rungchai990@gmail.com>
2021-10-08 11:16:27 +0700

    add webhook

--- server.js
+++ server.js
@@ -5,7 +5,24 @@ const PORT = process.env.PORT || 3000
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
- 
-app.listen(PORT,function(){
+app.get('/webhook', function (req, res) {
+  try {
+    const data = [
+      {
+        name: "rungchai",
+        age: 23
+      },
+      {
+        name: "ball",
+        age: 23
+      }
+    ]
+    res.status(200).json(data);
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+app.listen(PORT, function () {
   console.log(`Start Server Port 3000`)
 })
