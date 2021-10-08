08eae53e2589933803d0c3f7a05e34dfcd271ce0
rungtom003 <rungchai990@gmail.com>
2021-10-08 11:54:31 +0700

    edit full

--- server.js
+++ server.js
@@ -1,41 +1,70 @@
-const express = require('express')
+const https = require("https")
+const express = require("express")
 const app = express()
-const middleware = require('@line/bot-sdk').middleware
-const JSONParseError = require('@line/bot-sdk').JSONParseError
-const SignatureValidationFailed = require('@line/bot-sdk').SignatureValidationFailed
-const dotenv = require('dotenv');
-dotenv.config();
+require('dotenv').config()
 const PORT = process.env.PORT || 3000
+const TOKEN = process.env.LINE_ACCESS_TOKEN
+app.use(express.json())
+app.use(express.urlencoded({
+  extended: true
+}))
 
-const config = {
-  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
-  channelSecret: process.env.CHANNEL_SECRET
-}
-app.use(middleware(config))
-
-
-app.get('/', function (req, res) {
-  res.send('Hello World')
+app.get("/", (req, res) => {
+  res.sendStatus(200)
 })
-app.get('/webhook', function (req, res) {
-  try {
-    res.json(req.body.events) // req.body will be webhook event object
-  } catch (error) {
-    console.log(error)
+
+app.post("/webhook", function(req, res) {
+  res.send("HTTP POST request sent to the webhook URL!")
+  // If the user sends a message to your bot, send a reply message
+  if (req.body.events[0].type === "message") {
+    // Message data, must be stringified
+    const dataString = JSON.stringify({
+      replyToken: req.body.events[0].replyToken,
+      messages: [
+        {
+          "type": "text",
+          "text": "Hello, user"
+        },
+        {
+          "type": "text",
+          "text": "May I help you?"
+        }
+      ]
+    })
+
+    // Request header
+    const headers = {
+      "Content-Type": "application/json",
+      "Authorization": "Bearer " + TOKEN
+    }
+
+    // Options to pass into the request
+    const webhookOptions = {
+      "hostname": "api.line.me",
+      "path": "/v2/bot/message/reply",
+      "method": "POST",
+      "headers": headers,
+      "body": dataString
+    }
+
+    // Define request
+    const request = https.request(webhookOptions, (res) => {
+      res.on("data", (d) => {
+        process.stdout.write(d)
+      })
+    })
+
+    // Handle error
+    request.on("error", (err) => {
+      console.error(err)
+    })
+
+    // Send data
+    request.write(dataString)
+    request.end()
   }
 })
 
-app.use((err, req, res, next) => {
-  if (err instanceof SignatureValidationFailed) {
-    res.status(401).send(err.signature)
-    return
-  } else if (err instanceof JSONParseError) {
-    res.status(400).send(err.raw)
-    return
-  }
-  next(err) // will throw default 500
-})
-
-app.listen(PORT, function () {
-  console.log(`Start Server http://localhost:${PORT}`)
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`)
 })
